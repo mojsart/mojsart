9afc6f5f5dd42c245813cf4cc12534faff58dead
Davis Kim <dchk90@gmail.com>
2014-05-21 22:26:32 -0700

    Add d3 test example

--- client/common/directives.js
+++ client/common/directives.js
@@ -6,5 +6,37 @@
     return {
 
     };
+  })
+/*  .directive('pieChart', function() {
+    // Return our directive description object
+    // that will create the directive
+    return {
+      restrict: 'EA',
+      require: 'ngModel',
+      scope: {
+        'ngModel': '='
+      },
+      link: function(scope, element, attrs, ctrl) {
+        // Define our D3 components
+        var svg = d3.select(element[0])
+          .append('svg')
+          .attr('width', 400)
+          .attr('height', 400)
+          .append('g')
+          .attr('transform', 'translate(200,200)');
+
+        var color = d3.scale.category10(),
+            arc = d3.svg.arc().outerRadius(120).innerRadius(90),
+            pie = d3.layout.pie();
+
+        var arcs = svg.selectAll('path')
+          .data(pie(scope.ngModel))
+          .enter().append('path').attr({
+            d: arc,
+            fill: function(d,i) { return color(i); },
+            stroke: 'white'
+          });
+      }
+    };
   });
-}(angular));
+*/}(angular));
