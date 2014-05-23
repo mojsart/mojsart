84868d8dfb42c95c362fd0ca0b98158ec81fc3d4
Davis Kim <dchk90@gmail.com>
2014-05-22 21:25:26 -0700

    Get angular-d3 integration working

--- client/common/directives.js
+++ client/common/directives.js
@@ -102,66 +102,62 @@
       },
       link: function(scope, iElement, iAttrs) {
         d3.d3().then(function(d3) {
-          console.log('visualizer link running')
+          // console.log('visualizer link running')
           var svg = d3.select(iElement[0])
               .append("svg")
               .attr("width", "100%")
-              .attr("height", "500px");
+              .attr("height", "100%"); //TODO do not hardcode
 
-          // on window resize, re-render d3 canvas
-          window.onresize = function() {
-            return scope.$apply();
-          };
-          scope.$watch(function(){
-              return angular.element(window)[0].innerWidth;
-            }, function(){
-              return scope.render(scope.data);
-            }
-          );
+        //   // on window resize, re-render d3 canvas
+        //   window.onresize = function() {
+        //     return scope.$apply();
+        //   };
+        //   scope.$watch(function(){
+        //       return angular.element(window)[0].innerWidth;
+        //     }, function(){
+        //       return scope.render(scope.data);
+        //     }
+        //   );
 
-          // watch for data changes and re-render
+        //   // watch for data changes and re-render
           scope.$watch('data', function(newVals, oldVals) {
-            console.log(newVals)
             return scope.render(newVals);
           }, true);
 
-          // define render function
+        //   // define render function
           scope.render = function(data){
             // remove all previous items before render
             svg.selectAll("*").remove();
 
-            // setup variables
+        //     // setup variables
             var width, height, max;
             width = d3.select(iElement[0])[0][0].offsetWidth - 20;
-              // 20 is for margins and can be changed
-            height = 500;
-              // 35 = 30(bar height) + 5(margin between bars)
-            // max = 98;
-              // this can also be found dynamically when the data is not static
-              // max = Math.max.apply(Math, _.map(data, ((val)-> val.count)))
+        //       // 20 is for margins and can be changed
+            height = d3.select(iElement[0])[0][0].offsetHeight - 20;
+        //       // 35 = 30(bar height) + 5(margin between bars)
+        //     // max = 98;
+        //       // this can also be found dynamically when the data is not static
+        //       // max = Math.max.apply(Math, _.map(data, ((val)-> val.count)))
 
-            // set the height based on the calculations above
-            svg.attr('height', height);
+        //     // set the height based on the calculations above
+            // svg.attr('height', height);
 
-            //create the rectangles for the bar chart
+        //     //create the rectangles for the bar chart
             svg.selectAll("circle")
               .data(data)
               .enter()
                 .append("circle")
                 .on("click", function(d, i){return scope.onClick({item: d});})
                 .attr("r", 0) // height of each bar
-                .attr("cx",function() { return 1000*Math.random(); })
-                .attr("cy", function() { return 300*Math.random(); })
+                .attr("cx",function() { return width*Math.random(); })
+                .attr("cy", function() { return height*Math.random(); })
                 .transition()
                   .duration(1000) // time of duration
                 .attr("r", 20)
-                .attr("cx",function(d) { return 1000*(d.speechiness + d.acousticness/2); })
-                .attr("cy", function(d) { return 300*(d.energy + d.danceability)/2; });
-
-
+                .attr("cx",function(d) { return width*(d.speechiness + d.acousticness/2); })
+                .attr("cy", function(d) { return height*(d.energy + d.danceability)/2; });
           };
-        }
-        );
+        });
       }
     };
   }]);
