0e81416f05c0de95ab298f2cc14f9de5f78fe423
Taylor Harwin <taylor.harwin@gmail.com>
2014-05-23 18:01:13 -0700

    Add table of song/artist info to sidebar

--- client/sidebar/sidebar.js
+++ client/sidebar/sidebar.js
@@ -9,10 +9,22 @@ angular.module('mojsart.main.sidebar', ['ui.router'])
       controller: 'SideBarController'
     });
 })
-.controller('SideBarController', function ($scope) {
-  console.log('inside  sidepanel Controller');
+
+.controller('SideBarController', function ($scope, $http) {
   $scope.buttonStatus =  'full';
-  $scope.songs = ['Let it be','Run Like an Antelope','Killer Queen'];
+  $scope.songs = [];
+  $scope.quantity = 5;
+
+
+//Fetches all songs when app starts, loads Track and Title into Songs Array
+  $http.get('/song')
+    .success(function(json) {
+      $scope.data = json;
+      console.log($scope.data[0].echoData.artist);
+      for (var i = 0; i < $scope.data.length; i++){
+        $scope.songs.push({'track': $scope.data[i].echoData.artist, 'title': $scope.data[i].echoData.title});
+      }
+    });
 });
 
 
