b4326d0f278f7d30691dcbd62b77689c93fce2cd
Taylor Harwin <taylor.harwin@gmail.com>
2014-05-29 18:14:55 -0700

    close with file upload

--- client/main/main.js
+++ client/main/main.js
@@ -9,7 +9,7 @@
     'fx.animations',
     'ngAnimate',
   ])
-
+  
   .config(function ($stateProvider) {
     $stateProvider
     .state('mojsart.main', {
@@ -51,14 +51,42 @@
         $scope.sharedState.data = json;
       });
     };
-
     // TODO: make it so that we don't need to "initialize" like this
     $scope.sharedState = {};
     $scope.getSongs();
-    $scope.formData = {};
-
-    $scope.sendSong = function(){
-      console.log($scope.formData);
+    $scope.files = {};
+    //recognizes when one or more files are selected, loads them to array
+    $scope.filesChanged = function(elm){
+      $scope.files = elm.files;
+      $scope.$apply();
+      console.log($scope.files);
     };
-  });
+    //Loops over $scope.files object, formats each file as FormData
+      $scope.upload = function () {
+      console.log($scope.files);
+      var fd = new FormData();
+      angular.forEach($scope.files, function (file) {
+          fd.append('file', file);
+      });
+
+      //This is the Post request to add a new song
+      $http({
+          method: 'POST',
+          url: '/song/send',
+          data: fd,
+          headers: {
+              'Content-Type': undefined
+          },
+          transformRequest: angular.identity
+      })
+          .success(function (data, status, headers, config) {
+          console.log("Sent:", data);
+      });
+  };
+});
+
 })(angular);
+
+
+
+
