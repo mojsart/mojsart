35f762c60c28a0de84bec2de2e2b618aacd9058e
Jonathan Huang <jon.hc.huang@gmail.com>
2014-06-03 10:39:12 -0700

    Fixing deployment bug

--- server/main/demoInit.js
+++ server/main/demoInit.js
@@ -1,8 +1,8 @@
 var Song = require('../song/song_model.js'),
     Q    = require('q'),
     echo = require('./echo.js'),
-    SongHelpers = require('../song/song_helpers.js');
+    upload = require('../song/song_upload.js');
 
 module.exports = exports = function(req, res) {
-  SongHelpers.uploadSongs();
+  upload.uploadSongs();
 };
