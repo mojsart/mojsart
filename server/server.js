4d4d4ee28584ee964cd0c8457383edb5193fc374
Davis Kim <dchk90@gmail.com>
2014-06-02 18:18:20 -0700

    Stub out server

--- server/server.js
+++ server/server.js
@@ -1,27 +1,34 @@
-"use strict";
+// "use strict";
 
-/*
- *
- * Entry file into the server
- * @app -
- *    our express app. Exported for testing and flexibility.
- *
-*/
+// /*
+//  *
+//  * Entry file into the server
+//  * @app -
+//  *    our express app. Exported for testing and flexibility.
+//  *
+// */
 
-var app   = require('./main/app.js'),
-    http = require('http').Server(app),
-    io = require('socket.io')(http),
-    port  = app.get('port'),
-    log   = 'Listening on ' + app.get('base url') + ':' + port;
+// var app   = require('./main/app.js'),
+//     http = require('http').Server(app),
+//     io = require('socket.io')(http),
+//     port  = app.get('port'),
+//     log   = 'Listening on ' + app.get('base url') + ':' + port;
 
-http.listen(port);
-console.log(log);
+// http.listen(port);
+// console.log(log);
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+// io.on('connection', function(socket){
+//   console.log('a user connected');
 
-  socket.on('postUserData', function() {
-    console.log('postUserData')
-    io.emit('reget');
-  });
-});
+//   socket.on('postUserData', function() {
+//     console.log('postUserData')
+//     io.emit('reget');
+//   });
+// });
+
+var http = require('http')
+var port = process.env.PORT || 9000;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
