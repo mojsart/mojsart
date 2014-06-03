2cdd39103de9a0c8d1de04d46074b73f7293069d
Taylor Harwin <taylor.harwin@gmail.com>
2014-06-02 17:31:11 -0700

    disappearing tutorial for new user

--- client/sidebar/sidebar.js
+++ client/sidebar/sidebar.js
@@ -24,7 +24,7 @@ angular.module('mojsart.main.sidebar', ['ui.router', 'fx.animations',
     $scope.getSongs();
   });
 
-  $scope.sharedState.fillSongsList();
+  // $scope.sharedState.fillSongsList();
   
 
 
