255a041d5b9bb5c070acec96b713031de86dc7aa
Jonathan Huang <jon.hc.huang@gmail.com>
2014-06-05 17:28:18 -0700

    Uncomment npm install from build file.

--- deploy.sh
+++ deploy.sh
@@ -105,24 +105,24 @@ selectNodeVersion
 
 ##### commented out this block so deploys don't take forever
 
-# cd "$DEPLOYMENT_TARGET"
-# echo 2. Install npm packages
-# if [ -e "package.json" ]; then
-#   eval $NPM_CMD install --production
-#   exitWithMessageOnError "npm failed"
-#   echo "Execute gulp tasks"
-#   eval /node_modules/.bin/gulp build
-#   cd - > /dev/null
-# fi
+cd "$DEPLOYMENT_TARGET"
+echo 2. Install npm packages
+if [ -e "package.json" ]; then
+  eval $NPM_CMD install --production
+  exitWithMessageOnError "npm failed"
+  echo "Execute gulp tasks"
+  eval /node_modules/.bin/gulp build
+  cd - > /dev/null
+fi
 
-# if [ -e "$DEPLOYMENT_TARGET/bower.json" ]; then
-#   cd "$DEPLOYMENT_TARGET"
-#   eval $NPM_CMD install bower
-#   exitWithMessageOnError "installing bower failed"
-#   ./node_modules/.bin/bower install
-#   exitWithMessageOnError "bower failed"
-#   cd - > /dev/null
-# fi
+if [ -e "$DEPLOYMENT_TARGET/bower.json" ]; then
+  cd "$DEPLOYMENT_TARGET"
+  eval $NPM_CMD install bower
+  exitWithMessageOnError "installing bower failed"
+  ./node_modules/.bin/bower install
+  exitWithMessageOnError "bower failed"
+  cd - > /dev/null
+fi
 
 if [ -e "$DEPLOYMENT_TARGET/Gulpfile.js" ]; then
   cd "$DEPLOYMENT_TARGET"
