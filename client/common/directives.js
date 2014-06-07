bc8b787e9d7fe235e5c6755ab36cb693b86113d1
Davis Kim <dchk90@gmail.com>
2014-06-06 23:04:26 -0700

    Fix render on no data

--- client/common/directives.js
+++ client/common/directives.js
@@ -4,14 +4,14 @@
 
   angular.module('mojsart')
   // tweet button directive for about page
-  .directive('twitter', ['$timeout', function($timeout) {
+  .directive('twitter', ['$timeout', function ($timeout) {
     return {
-      link: function(scope, element, attr) {
-        $timeout(function() {
+      link: function (scope, element, attr) {
+        $timeout(function () {
           twttr.widgets.createShareButton(
             attr.url,
             element[0],
-            function() {}, {
+            function () {}, {
               count: attr.count,
               text: attr.text,
               via: attr.via,
@@ -24,7 +24,7 @@
   }])
 
   // home visualizer directive
-  .directive('d3Visualizer', ['d3Service', function(d3Service) {
+  .directive('d3Visualizer', ['d3Service', function (d3Service) {
     return {
       restrict: 'EA',
       scope: {
@@ -33,32 +33,36 @@
         onClick: "&",
         setData: "="
       },  
-      link: function(scope, iElement, iAttrs) {
+      link: function (scope, iElement, iAttrs) {
         // initialize d3
-        d3Service.d3().then(function(d3) {
+        d3Service.d3().then(function (d3) {
           var svg = d3.select(iElement[0]).append("svg"); // select the parent div
           
-          window.onresize = function() {
+          window.onresize = function () {
             return scope.$apply();
           };
 
           // watch window height and re-render on changes
-          scope.$watch(function() {
+          scope.$watch(function () {
             return angular.element(window)[0].innerHeight;
-          }, function() {
-            svg.attr("height", function() {
+          }, function () {
+            svg.attr("height", function () {
               return angular.element(window)[0].innerHeight - 90; // re-set graph to window height - menubar space
             });
-            return scope.render(scope.data);
+            if(scope.data) {
+              return scope.render(scope.data);
+            }
           });
 
           // watch for data changes and re-render on changes
-          scope.$watch('data', function(newVals, oldVals) {
-            return scope.render(newVals);
+          scope.$watch('data', function (newVals, oldVals) {
+            if(newVals){
+              return scope.render(newVals);
+            }
           }, true);
 
           // define render function
-          scope.render = function(data){
+          scope.render = function (data) {
 
             // remove all previous items before render
             svg.selectAll("*").remove();
@@ -68,7 +72,7 @@
             var height = d3.select(iElement[0])[0][0].offsetHeight;
 
             // sort data so smaller circles appear on top
-            if(data.length > 1) {
+            if(data && data.length > 1) {
               data = data.sort(function (a, b) {
                 if (a.echoData.audio_summary.loudness > b.echoData.audio_summary.loudness) {
                   return 1;
@@ -90,7 +94,7 @@
                   .attr("stroke-width", 2);
 
             // make circles bigger and change color on mouseover
-            circles.on("mouseover", function() {
+            circles.on("mouseover", function () {
               d3.select(this)
                 .transition()
                 .attr("fill", "#9BDA6A")
@@ -98,10 +102,10 @@
             });
 
             // on click, set artist/title and run setData
-            circles.on("click", function(d, i) {
+            circles.on("click", function (d, i) {
               var artist = d.echoData.artist;
               var title = d.echoData.title;
-              scope.$apply(function() { scope.setData(d); });
+              scope.$apply(function () { scope.setData(d); });
 
               // var x = d3.select(this).attr('cx');
               // var y = d3.select(this).attr('cy');
@@ -110,20 +114,20 @@
             });
 
             // on mouseout, make circle back to normal size/color
-            circles.on("mouseout", function() {
+            circles.on("mouseout", function () {
               d3.select(this)
                 .transition()
-                .attr("fill", function(d) { return (d.cached) ? "#60A859" : "white"; })
-                .attr("r", function(d) { return 40*Math.pow(10, (d.echoData.audio_summary.loudness/20)); });
+                .attr("fill", function (d) { return (d.cached) ? "#60A859" : "white"; })
+                .attr("r", function (d) { return 40*Math.pow(10, (d.echoData.audio_summary.loudness/20)); });
             });
 
             // grow circles to appropriate size
             circles.transition()
               .duration(1000) // time of duration
-            .attr("r", function(d) { return 40*Math.pow(10, (d.echoData.audio_summary.loudness/20)); })
-            .attr("cx",function(d) { return width*(d.echoData.audio_summary.speechiness + d.userData.speechiness + d.echoData.audio_summary.acousticness/3); })
-            .attr("cy", function(d) { return height*(d.echoData.audio_summary.energy + d.echoData.audio_summary.danceability)/2; })
-            .attr("fill", function(d) { return (d.cached) ? "#60A859" : "white"; });
+            .attr("r", function (d) { return 40*Math.pow(10, (d.echoData.audio_summary.loudness/20)); })
+            .attr("cx",function (d) { return width*(d.echoData.audio_summary.speechiness + d.userData.speechiness + d.echoData.audio_summary.acousticness/3); })
+            .attr("cy", function (d) { return height*(d.echoData.audio_summary.energy + d.echoData.audio_summary.danceability)/2; })
+            .attr("fill", function (d) { return (d.cached) ? "#60A859" : "white"; });
           };
         });
       }
