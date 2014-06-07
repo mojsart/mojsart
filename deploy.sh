fde89445103356ddd5c9100adda41adce54cbaa9
Jonathan Huang <jon.hc.huang@gmail.com>
2014-06-06 20:32:36 -0700

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -106,21 +106,21 @@ selectNodeVersion
 ##### commented out this block so deploys don't take forever
 
 # echo 3. Install npm packages
-cd "$DEPLOYMENT_TARGET"
-if [ -e "package.json" ]; then
-  eval $NPM_CMD install --production
-  exitWithMessageOnError "npm failed"
-  cd - > /dev/null
-fi
+# cd "$DEPLOYMENT_TARGET"
+# if [ -e "package.json" ]; then
+#   eval $NPM_CMD install --production
+#   exitWithMessageOnError "npm failed"
+#   cd - > /dev/null
+# fi
 
-if [ -e "$DEPLOYMENT_TARGET/bower.json" ]; then
-  cd "$DEPLOYMENT_TARGET"
-  eval $NPM_CMD install bower
-  exitWithMessageOnError "installing bower failed"
-  ./node_modules/.bin/bower install
-  exitWithMessageOnError "bower failed"
-  cd - > /dev/null
-fi
+# if [ -e "$DEPLOYMENT_TARGET/bower.json" ]; then
+#   cd "$DEPLOYMENT_TARGET"
+#   eval $NPM_CMD install bower
+#   exitWithMessageOnError "installing bower failed"
+#   ./node_modules/.bin/bower install
+#   exitWithMessageOnError "bower failed"
+#   cd - > /dev/null
+# fi
 
 if [ -e "$DEPLOYMENT_TARGET/Gulpfile.js" ]; then
   cd "$DEPLOYMENT_TARGET"
